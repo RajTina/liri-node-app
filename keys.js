cbef0d94fc1adda67e58dd40888e6f94dba67ca1
Tina Azad <mktazad@gmail.com>
2019-08-13 17:57:22 -0400

    added api key

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
-
+require ('dotenv').config();
 exports.spotify = {
-  id: 'process.env.SPOTIFY_3635a242b1d340ec92a859b568e14fid',
+  id: process.env.SPOTIFY_ID,
   // process.env.SPOTIFY_ID,
-  secret: 'process.env.SPOTIFY_3c5f2ab41cb44cd7acd3686de1f67127'
+  secret: process.env.SPOTIFY_SECRET
   // process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -2,25 +2,55 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
 var moment = require("moment");
+var axios = require("axios");
 var fs = require("fs");
-var spotify = require("spotify");
-var spotify = require("node-spotify-api");
+//var spotify = require("spotify");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var input = process.argv[3];
 
-//var getSpotify = ffunction () {
+//var getSpotify = function () {
 
+
+ var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+});
+   
+//   spotify.search ({ type: 'track', query: 'All the Small Things'}, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+//   console.log(data); 
+//   });
+
+//   let artist = data.tracks.items[i].album.artists[0].name; 
+//   console.log(artist);
 //}
-var spotify = new Spotify({
-     id: 'process.env.SPOTIFY_3635a242b1d340ec92a859b568e14fid',
-     secret:'process.env.SPOTIFY_3c5f2ab41cb44cd7acd3686de1f67127'
-   });
-   
-  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
     }
-   
-  console.log(data); 
+    console.log(error.config);
   });
